61a4b694dd2505b5668c736833fd29ed2c1345de
Andrii Miroshnichenko <andy.miroshnichenko@gmail.com>
2021-11-03 22:42:59 +0200

    fixed task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,56 +6,89 @@ const galleryContainer = document.querySelector('.gallery');
 // Create gallery.
 const createItem = galleryItems
   .map(
-    (el) =>
+    (el, index) =>
       `<div class="gallery__item">
-     <a class="gallery__link" href="${el.original}">
-     <img class="gallery__image"
-          src="${el.preview}"
-          data-source="${el.original}"
-          alt="${el.description}"
-      />
-      </a>
-    </div>`,
+  <a class="gallery__link" href="${el.original}">
+    <img
+      class="gallery__image"
+      src="${el.preview}"
+      data-source="${el.original}"
+      data-index="${index}"
+      alt="${el.description}"
+    />
+  </a>
+</div>`,
   )
   .join('');
-
 galleryContainer.insertAdjacentHTML('beforeend', createItem);
-
-// On modal window
 galleryContainer.addEventListener('click', onSelectImage);
 
-// 1) вариант:
+// Example 1:
+// On modal window
+// function onSelectImage(e) {
+//   e.preventDefault();
+
+//   if (e.target.nodeName !== 'IMG') {
+//     return;
+//   }
+
+//   const onSelectedImage = e.target.dataset.source;
+//   // Connecting the library basicLightbox:
+//   const instance = basicLightbox.create(
+//     `<img src="${onSelectedImage}" width="800" height="600">`,
+//   );
+//   instance.show();
+//   console.log('Modal open, listener added');
+
+//   window.addEventListener('keydown', handleKeypress);
+
+//   function handleKeypress(e) {
+//     if (e.code === 'Escape') {
+//       instance.close();
+//       console.log(e.code);
+//       closeImgModal();
+//     }
+//   }
+
+//   function closeImgModal() {
+//     window.removeEventListener('keydown', handleKeypress);
+//     console.log('Listener removed');
+//   }
+// }
+
+// Example 2:
+// Connecting the library basicLightbox:
+
 function onSelectImage(e) {
   e.preventDefault();
-
   if (e.target.nodeName !== 'IMG') {
     return;
   }
 
-  const onSelectedImage = e.target.dataset.source;
-  // Подключение библиотеки basicLightbox:
   const instance = basicLightbox.create(
-    `<img src="${onSelectedImage}" width="800" height="600">`,
+    `<img class="modal-img" src="">`,
+    {
+      onShow: (instance) => {
+        window.addEventListener('keydown', handleKeypress);        
+      },
+    },
+    {
+      onClose: (instance) => {
+        window.removeEventListener('keydown', handleKeypress);        
+      },
+    },
   );
-  instance.show();
-  console.log('Modal open, listener added');
 
-  window.addEventListener('keydown', handleKeypress);
+  instance.element().querySelector('.modal-img').src = e.target.dataset.source;
+  instance.show();
 
   function handleKeypress(e) {
     if (e.code === 'Escape') {
-      instance.close();
-      console.log(e.code);
-      closeImgModal();
+      instance.close();      
+      return;
     }
   }
-
-  function closeImgModal() {
-    window.removeEventListener('keydown', handleKeypress);
-    console.log('Listener removed');
-  }
 }
-
 
 
 // План выполнения задачи:
